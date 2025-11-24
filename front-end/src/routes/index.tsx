2a737e90c0517f603a48c1961b45c89030c809bd
OAndreae <oliverandreae@outlook.com>
2025-11-24 10:41:53 +0000

    Made form narrower.

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -9,11 +9,11 @@ export const Route = createFileRoute('/')({
 function HomePage() {
   return (
     <div className="container mx-auto px-6 py-8">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 h-[calc(100vh-12rem)]">
-        <div className="h-full overflow-hidden">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 h-[calc(100vh-12rem)]">
+        <div className="lg:col-span-1 h-full overflow-hidden">
           <PodcastForm />
         </div>
-        <div className="h-full overflow-hidden">
+        <div className="lg:col-span-2 h-full overflow-hidden">
           <PreparationNotesDisplay />
         </div>
       </div>
